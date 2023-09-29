06768849aa1748c3f9c7564a7bff2e2d7113a710
Pooranjoy Bhattacharya <pooranjoyb2016@gmail.com>
2023-09-29 11:32:44 +0530

    update v2.0.1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,17 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 import { TodoComponent } from './todo/todo.component';
+import { FruitsComponentComponent } from './fruits-component/fruits-component.component';
+import { VegComponentComponent } from './veg-component/veg-component.component';
+import { DataserviceService } from './services/dataservice.service';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TodoComponent
+    TodoComponent,
+    FruitsComponentComponent,
+    VegComponentComponent
   ],
   imports: [
     BrowserModule,
     FormsModule
   ],
-  providers: [],
+  providers: [
+    DataserviceService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
